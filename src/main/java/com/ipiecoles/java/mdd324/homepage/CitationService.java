fb54ee8456a5c4f97ab26ab94b7e2361f83de5d5
Folly Koudouovoh <koudouovohfolly@gmail.com>
2022-03-02 15:47:37 +0100

    citation

---
+++ src/main/java/com/ipiecoles/java/mdd324/homepage/CitationService.java
@@ -0,0 +1,4 @@
+package com.ipiecoles.java.mdd324.homepage;
+
+public class CitationService {
+}
--- src/main/java/com/ipiecoles/java/mdd324/homepage/Main.java
+++ src/main/java/com/ipiecoles/java/mdd324/homepage/Main.java
@@ -15,17 +15,26 @@ public class Main {
 
 
 
-
+    //JSON
         Genson genson = new GensonBuilder().useRuntimeType(true).create();
         String qod = Utils.getPageContents("https://gist.githubusercontent.com/pjvilloud/7c22281ce6a96a969bcb5b34e1383185/raw/1c6108961d6aae648f12f16b4a865c6e40918a4d/qod.json");
         Map<String, Object> map = genson.deserialize(qod, Map.class);
-        String author = (String) map.get("baseurl");
+
 
         HashMap<String, Object> contents = (HashMap<String, Object>) map.get("contents");
         ArrayList quotes = (ArrayList) contents.get("quotes");
         HashMap<String, Object> quote = (HashMap<String, Object>) quotes.get(0);
         System.out.println(quote.get("quote"));
         System.out.println(quote.get("author"));
+        Citation citation = new Citation(quote.get("author"));
+
+
+        //Entité Java => JSON
+        Genson g = new GensonBuilder().useRuntimeType(true).create();
+        String jsonOutput = g.serialize(citation);
+        System.out.println(jsonOutput);
+
+
 
     }
 
